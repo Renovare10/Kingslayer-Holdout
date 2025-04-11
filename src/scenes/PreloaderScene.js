f2f7cb969457747ff9dd5b6acfe64dac08505156
Chadfire <chadfire10@yahoo.com>
2025-04-11 19:08:47 -0400

    Update for building the project

--- src/scenes/PreloaderScene.js
+++ src/scenes/PreloaderScene.js
@@ -7,11 +7,9 @@ export default class PreloaderScene extends Phaser.Scene {
 
     preload() {
         for (let i = 0; i < 20; i++) {
-            this.load.image(`survivor-idle_handgun_${i}.png`, `/assets/Top_Down_Survivor/handgun/idle/survivor-idle_handgun_${i}.png`);
+            this.load.image(`survivor-idle_handgun_${i}.png`, `assets/Top_Down_Survivor/handgun/idle/survivor-idle_handgun_${i}.png`);
         }
-
-            // Add zombie asset loading
-            this.load.image('zombie', 'assets/Zombie/zombie.png');
+        this.load.image('zombie', 'assets/Zombie/zombie.png');        
     }
 
     create() {
