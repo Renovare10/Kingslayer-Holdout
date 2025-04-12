2f2efbb90ca26ad20160f6519fb08d7ae931be50
Chadfire <chadfire10@yahoo.com>
2025-04-12 12:32:37 -0400

    Player has bounding box

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -1,3 +1,4 @@
+// src/entities/Player.js
 import Position from '../components/Position.js';
 import Sprite from '../components/Sprite.js';
 import { createRotateToMouse } from '../components/RotateToMouse.js';
@@ -7,13 +8,17 @@ import { createEntityType } from '../components/EntityType.js';
 
 export default function createPlayer(ecs, scene, x, y) {
   const playerId = ecs.createEntity();
-  
   ecs.addComponent(playerId, 'position', new Position(x, y));
-  ecs.addComponent(playerId, 'sprite', new Sprite(scene, x, y, 'survivor-idle_handgun_0'));
+  const sprite = new Sprite(scene, x, y, 'survivor-idle_handgun');
+  sprite.phaserSprite.setOrigin(0.5);
+  sprite.phaserSprite.body.setCircle(85); // Radius = half of 170
+  sprite.phaserSprite.body.setOffset(40, 25); // Keep existing offset
+  ecs.addComponent(playerId, 'sprite', sprite);
   ecs.addComponent(playerId, 'rotatetomouse', createRotateToMouse());
   ecs.addComponent(playerId, 'movement', createMovement(200));
   ecs.addComponent(playerId, 'shooting', createShooting(0.2));
   ecs.addComponent(playerId, 'entityType', createEntityType('player'));
-  
+  ecs.addComponent(playerId, 'physicsBody', { body: sprite.phaserSprite.body });
+  ecs.initEntity(playerId);
   return playerId;
 }
